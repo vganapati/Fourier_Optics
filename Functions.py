0bed7b92860bb3360da20e31fe0ad5519495c0d3
Vidya Ganapati <vganapa1@swarthmore.edu>
2021-01-06 13:29:36 -0500

    added propagators

--- Functions.py
+++ Functions.py
@@ -7,3 +7,137 @@ Created on Wed Jan  6 10:12:44 2021
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+def F(mat2D): # Fourier transform assuming center pixel as center
+    return np.fft.fftshift(np.fft.fft2(mat2D))
+
+def Ft(mat2D): # Inverse Fourier transform assuming center pixel as center
+    return np.fft.ifft2(np.fft.ifftshift(mat2D))
+
+def make_meshgrid(dx = 0.01,
+                  dy = 0.01,
+                  Lx_max = 1,
+                  Ly_max = 1):
+    
+    Lx_min = -Lx_max
+    Ly_min = -Ly_max
+    x = np.arange(Lx_min,Lx_max,dx)
+    y = np.arange(Ly_min,Ly_max,dy)
+    xm, ym = np.meshgrid(x,y, indexing='ij')
+    extent = [Lx_min, Lx_max-dx, Ly_min, Ly_max-dy]
+    
+    # Fourier transform dimensions
+    F_extent = [len(x)/(4*Lx_min), len(x)/(4*Lx_max) - 1/(Lx_max*2), 
+                len(y)/(4*Ly_min), len(y)/(4*Ly_max) - 1/(Ly_max*2)]
+    
+    return(x,y,xm,ym,extent,F_extent)
+
+def make_circle(dx = 0.01,
+                dy = 0.01,
+                Lx_max = 1,
+                Ly_max = 1,
+                a = 1, # stretch in x
+                b=1, # stretch in y
+                ):
+    
+    x,y,xm,ym,extent,F_extent = make_meshgrid(dx,
+                                              dy,
+                                              Lx_max,
+                                              Ly_max)
+
+    circle = np.zeros_like(xm)
+    circle[((a*xm)**2+(b*ym)**2) < 1] = 1
+    circle[((a*xm)**2+(b*ym)**2) == 1] = 0.5
+
+    F_circle = F(circle)
+    
+    return(circle, extent, F_circle, F_extent)
+
+def plot(img, extent):
+
+    plt.figure()
+    plt.title('Magnitude')
+    plt.imshow(np.abs(img), interpolation='none', extent = extent, cmap='gray')
+    plt.colorbar()
+
+    plt.figure()
+    plt.title('Phase')
+    plt.imshow(np.angle(img), interpolation='none', extent = extent, cmap='hsv',
+               vmin = -np.pi, vmax = np.pi)
+    plt.colorbar()
+        
+def rect(mesh):
+    output = np.zeros_like(mesh)
+    output[np.abs(mesh)<0.5] = 1
+    output[np.abs(mesh)==0.5] = 0.5
+    return(output)
+    
+def triangle(mesh):
+    output = np.zeros_like(mesh)
+    output[np.abs(mesh)<=1] = 1 - np.abs(mesh[np.abs(mesh)<=1])
+    return(output)
+
+def sgn(mesh):
+    output = np.zeros_like(mesh)
+    output[mesh>0] = 1
+    output[mesh<0] = -1
+    return(output)
+
+def comb(mesh):
+    output = np.zeros_like(mesh)
+    output[np.abs(mesh % 1) <= 1e-10] = 1
+    return(output)
+
+exp_func_0 = lambda mesh: np.exp(-np.pi*mesh**2)
+exp_func_1 = lambda mesh: np.exp(1j*np.pi*mesh)
+exp_func_2 = lambda mesh: np.exp(1j*np.pi*mesh**2)
+exp_func_3 = lambda mesh: np.exp(-(np.abs(mesh)))
+
+def make_2D(dx = 0.01,
+            dy = 0.01,
+            Lx_max = 1,
+            Ly_max = 1,
+            a = 1,
+            b=1,
+            func=rect # options are rect, triangle, sgn, comb, or exp_func_n
+            ):
+    
+    x,y,xm,ym,extent,F_extent = make_meshgrid(dx,
+                                              dy,
+                                              Lx_max,
+                                              Ly_max)
+
+    mat2D = func(a*xm)*func(b*ym)
+        
+    return(mat2D, extent, F(mat2D), F_extent)
+
+
+def make_2D_delta(dx = 0.01,
+                  dy = 0.01,
+                  Lx_max = 1,
+                  Ly_max = 1,
+                  a = 1,
+                  b=1,
+                  ):
+    
+    x,y,xm,ym,extent,F_extent = make_meshgrid(dx,
+                                              dy,
+                                              Lx_max,
+                                              Ly_max)
+    delta_x = np.zeros_like(xm)
+    delta_y = np.zeros_like(xm)
+    delta_x[np.abs(xm)<1e-10]=1/np.abs(a)
+    delta_y[np.abs(ym)<1e-10]=1/np.abs(b)
+    
+    mat2D = delta_x*delta_y
+        
+    return(mat2D, extent, F(mat2D), F_extent)
+
+if __name__ == "__main__":
+    func = exp_func_0
+    mat2D, extent, F_mat2D, F_extent = make_2D(func=func)
+    plot(mat2D, extent)
+
+    mat2D, extent, F_mat2D, F_extent = make_2D_delta()
+    plot(mat2D, extent)
---
+++ propagators.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 10:39:10 2021
+
+@author: vganapa1
+"""
+
+from functions import F, Ft, make_meshgrid, plot
+import numpy as np
+
+def scalar_prop(u0,dx,dy,z,wavelength): # x is the row coordinate, y is the column coordinate
+    Nx = u0.shape[0]
+    Ny = u0.shape[1]
+    Lx = Nx*dx
+    Ly = Ny*dy
+    
+    U0 = F(u0)
+    
+    fx=np.linspace(-1/(2*dx),1/(2*dx)-1/Lx,Nx) #freq coords
+    fy=np.linspace(-1/(2*dy),1/(2*dy)-1/Ly,Ny) #freq coords
+    
+    FX,FY=np.meshgrid(fx,fy, indexing = 'ij')
+
+    H = np.exp(1j*2*np.pi*(1./wavelength)*z*np.sqrt(1-(wavelength*FX)**2-(wavelength*FY)**2))
+    H[np.nonzero( np.sqrt(FX**2+FY**2) > (1./wavelength) )] = 0
+    
+    U1 = U0*H
+    u1 = Ft(U1)
+    
+    return(u1)
+
+def fresnel_prop(u0,dx,dy,z,wavelength): # x is the row coordinate, y is the column coordinate
+    Nx = u0.shape[0]
+    Ny = u0.shape[1]
+    Lx = Nx*dx
+    Ly = Ny*dy
+    
+    U0 = F(u0)
+    
+    fx=np.linspace(-1/(2*dx),1/(2*dx)-1/Lx,Nx) #freq coords
+    fy=np.linspace(-1/(2*dy),1/(2*dy)-1/Ly,Ny) #freq coords
+    
+    FX,FY=np.meshgrid(fx,fy, indexing = 'ij')
+
+    H_fresnel=np.exp(-1j*np.pi*wavelength*z*(FX**2+FY**2))
+    H_fresnel[np.nonzero( np.sqrt(FX**2+FY**2) > (1./wavelength) )] = 0
+    
+    U1 = U0*H_fresnel
+    u1 = Ft(U1)
+    
+    return(u1)
+
+def create_plane_wave(dx, dy,
+                      Lx_max, Ly_max, 
+                      alpha, beta, wavelength):
+    
+    fx = alpha/wavelength
+    fy = beta/wavelength
+    
+    x,y,xm,ym,extent,F_extent = make_meshgrid(dx,
+                                              dy,
+                                              Lx_max,
+                                              Ly_max)
+    
+    plane_wave = np.exp(1j*2*np.pi*(fx*xm + fy*ym))
+    return(plane_wave)
+
+if __name__ == "__main__":
+    from functions import make_circle
+    
+    # units are microns
+    dx = 0.6
+    dy = 0.6
+    Lx_max = 1000
+    Ly_max = 1000
+    wavelength = .6
+    alpha =  1.2
+    beta = 1.2
+    z = 1000
+    
+    plane_wave = create_plane_wave(dx, dy,
+                                   Lx_max, Ly_max, 
+                                   alpha, beta, wavelength)
+   
+    
+    mat2D, extent, F_mat2D, F_extent = make_circle(dx = dx,
+                                                   dy = dy,
+                                                   Lx_max = Lx_max,
+                                                   Ly_max = Ly_max,
+                                                   a = .005, # stretch in x
+                                                   b=.005, # stretch in y
+                                                   )
+    plot(plane_wave, extent)
+    plot(mat2D, extent)
+    output_field = scalar_prop(mat2D*plane_wave,dx,dy,z,wavelength)
+    
+    
+    plot(output_field, extent)
+    
